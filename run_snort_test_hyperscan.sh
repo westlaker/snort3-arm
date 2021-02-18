19b8a05789a31fb4b73aa8c82eeb9036624df02a
root <root@altra2p.maas>
2021-02-18 21:07:37 +0000

    correct some typos

--- run_snort_test_hyperscan.sh
+++ run_snort_test_hyperscan.sh
@@ -1,2 +1,2 @@
-snort3.1.1.0 snort --rule-path /usr/local/etc/rules/ -c /usr/local/etc/snort/snort.lua --lua 'search_engine.search_method = "hyperscan"' -r /root/snort/maccdc2012_00001.pcap
+snort --rule-path /usr/local/etc/rules/ -c /usr/local/etc/snort/snort.lua --lua 'search_engine.search_method = "hyperscan"' -r /root/snort-pcaps/maccdc2012_00001.pcap
 
